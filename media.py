f1a3228b23eb38c354e3fad0db27889e508a2efb
Bryan Mutai <mutaiwork@gmail.com>
2018-05-27 20:17:14 +0100

    Ran the code through some linting

--- media.py
+++ media.py
@@ -1,10 +1,12 @@
-class Movie():
+class Movie:
     """
-     Definition of the movie object, holding all the required information about a movie
+     Definition of the movie object,
+      holding all the required information about a movie
     """
     VALID_RATINGS = ["G", "PG", "PG-13", "R"]
 
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+    def __init__(self, movie_title, movie_storyline, poster_image,
+                 trailer_youtube):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
